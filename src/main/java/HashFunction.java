78b4277285828f75dc6b9a899e641eecb2054d38
mt-cs <Mtz31891usfca19>
2021-04-14 19:52:42 -0700

    loadFactor added

--- src/main/java/HashFunction.java
+++ src/main/java/HashFunction.java
@@ -29,9 +29,22 @@ public class HashFunction {
         return bInt;
     }
 
-
+    /**
+     * Compress the bigInt value so that it is in the range from 0 to max_size
+     * @param bigInteger hash code of a string
+     * @return return key % max_size.
+     */
     public int hashFunction (BigInteger bigInteger) {
         return bigInteger.mod(BigInteger.valueOf(max_size)).intValue();
+    }
+
+    /**
+     * Calculating the load factor
+     * @param num_entries number of entries in the table
+     * @return the number of entries divided by the maximum size of the table.
+     */
+    public double loadFactor (int num_entries) {
+        return (double) num_entries / max_size;
     }
 
 }
--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -3,5 +3,6 @@ public class TestMain {
         HashFunction hf = new HashFunction(13);
         System.out.println(hf.hashCode("hello"));
         System.out.println(hf.hashFunction(hf.hashCode("hello")));
+        System.out.println(hf.loadFactor(7));
     }
 }
