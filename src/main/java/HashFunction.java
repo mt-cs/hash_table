f08f38891f12ba946ddf7bb37d9272aaba90fafe
mt-cs <Mtz31891usfca19>
2021-04-14 20:26:01 -0700

    prime helper function added

--- src/main/java/HashFunction.java
+++ src/main/java/HashFunction.java
@@ -1,3 +1,5 @@
+import hashTable.openHashing.Node;
+
 import java.math.BigInteger;
 
 public class HashFunction {
@@ -46,5 +48,46 @@ public class HashFunction {
     public double loadFactor (int num_entries) {
         return (double) num_entries / max_size;
     }
+
+    public void hashing (Node[] table) {
+        int new_size = closestPrime(max_size * 2);
+    }
+
+    /**
+     * Find the closest prime number.
+     * @param num is 2 * max_size
+     * @return the smallest prime number that is larger than num
+     */
+    public int closestPrime (int num) {
+        int prime_below = 0;
+        for (int i = num - 1; i >= 1; i--) {
+            if (isPrime(i)) {
+                prime_below =  i;
+                break;
+            }
+        }
+        int prime_after = prime_below + 1;
+        for (int i = num; i < num + prime_below; i++) {
+            if (isPrime(i)) {
+                prime_after = i;
+                break;
+            }
+        }
+        if (num - prime_below > prime_after - num) {
+            return prime_after;
+        } else {
+            return prime_below;
+        }
+    }
+
+    public boolean isPrime(int num) {
+        if (num == 2 || num == 3 || num == 5 || num == 7) {
+            return true;
+        } else if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0) {
+            return false;
+        } else return Math.ceil(Math.sqrt(num)) - Math.floor(Math.sqrt(num)) != 0;
+    }
+
+
 
 }
--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -4,5 +4,11 @@ public class TestMain {
         System.out.println(hf.hashCode("hello"));
         System.out.println(hf.hashFunction(hf.hashCode("hello")));
         System.out.println(hf.loadFactor(7));
+        System.out.println(hf.isPrime(3));
+        System.out.println(hf.isPrime(84));
+        System.out.println(hf.isPrime(101));
+        System.out.println(hf.closestPrime(100));
+        System.out.println(hf.closestPrime(90));
+        System.out.println(hf.closestPrime(6));
     }
 }
